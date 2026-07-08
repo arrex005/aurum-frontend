2df18172ec14664009b63e75096a2facfd41be81
arrex005 <alejandro.arrescavero@cieep.com>
2026-07-08 13:20:03 +0200

    Cambiar Blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import { useInView } from 'framer-motion'
 import { useRef } from 'react'
 
-
+const categorias = ['Todos', 'Historia', 'Oro', 'Plata', 'Platino']
 
 function ArticuloCard({ articulo, index }) {
   const ref = useRef(null)
